55173d7a393eb569e2507bbe75477f7f185d033a
Utkal97 <utkal1997@live.in>
2017-10-07 10:04:03 +0530

    Update views.py

--- maps/views.py
+++ maps/views.py
@@ -28,31 +28,31 @@ def housetable(request):
     jsondata=json.loads(response.read())
     return jsondata
     
-def members(request):
+def memberstable(request):
     url="https://10.0.3.23:8787/members/?format=json"
     response=urllib.urlopen(url)
     jsondata=json.loads(response.read())
     return jsondata
     
-def farmpoints(request):
+def farmpointstable(request):
     url="https://10.0.3.23:8787/farmpoints/?format=json"
     response=urllib.urlopen(url)
     jsondata=json.loads(response.read())
     return jsondata
     
-def crop(request):
+def croptable(request):
     url="https://10.0.3.23:8787/crop/?format=json"
     response=urllib.urlopen(url)
     jsondata=json.loads(response.read())
     return jsondata
     
-def wells(request):
+def wellstable(request):
     url="https://10.0.3.23:8787/wells/?format=json"
     response=urllib.urlopen(url)
     jsondata=json.loads(response.read())
     return jsondata
     
-def publicplaces(request):
+def publicplacestable(request):
     url="https://10.0.3.23:8787/publicplaces/?format=json"
     response=urllib.urlopen(url)
     jsondata=json.loads(response.read())
