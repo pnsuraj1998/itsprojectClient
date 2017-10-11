bc27ae40270c3000446fd7b958e4f0fda37e3167
Utkal97 <utkal1997@live.in>
2017-10-12 01:38:52 +0530

    Update settings.py

--- farmhelper/settings.py
+++ farmhelper/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # logging our apps
     'maps',
     'rest_framework',
     'rest_framework_gis',
